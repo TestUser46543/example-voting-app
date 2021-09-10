416300f4ea157e0a377258ad9006ca58911d8f48
KJ <Kartikeya.jain@skillassure.com>
2021-09-10 18:26:35 +0000

    fixed docker push error

--- buildImages.sh
+++ buildImages.sh
@@ -1,6 +1,6 @@
 cd vote
 docker build -t docker1234432156/vote-app .
-docker login -u "docker1234432156" -p "pasword123" docker.io
+docker login -u "docker1234432156" -p "password123" docker.io
 docker push docker1234432156/vote-app
 cd ..
 cd result
